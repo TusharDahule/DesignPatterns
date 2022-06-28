1fc78ed4e14c4ba123c30e4be1b95962cd4136c4
TusharDahule <tushardahule@gmail.com>
2022-06-28 13:18:21 +0530

    Builder Pattern added

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Design Patterns</h1>`;
 
 /* Builder pattern */
+console.log('---------Builder Pattern---------');
 const obj1 = {
   id: 1,
   name: 'Tushar',
 };
 
+/* adding properties to object with the help of function*/
 function addPropertyToObj(obj) {
   obj.addProp = () => {
-    console.log('Adding property to obj');
+    console.log('Adding property to obj using function...');
   };
 }
 
